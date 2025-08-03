d7fdf08ed81baed96883e3c57d244e89fe849ff7
Isaac Cook <16isaaccook@gmail.com>
2025-08-03 15:53:38 -0600

    remove about page

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -10,17 +10,11 @@
 
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as QuillRouteImport } from './routes/quill'
-import { Route as AboutRouteImport } from './routes/about'
 import { Route as IndexRouteImport } from './routes/index'
 
 const QuillRoute = QuillRouteImport.update({
   id: '/quill',
   path: '/quill',
-  getParentRoute: () => rootRouteImport,
-} as any)
-const AboutRoute = AboutRouteImport.update({
-  id: '/about',
-  path: '/about',
   getParentRoute: () => rootRouteImport,
 } as any)
 const IndexRoute = IndexRouteImport.update({
@@ -31,31 +25,27 @@ const IndexRoute = IndexRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
-  '/about': typeof AboutRoute
   '/quill': typeof QuillRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
-  '/about': typeof AboutRoute
   '/quill': typeof QuillRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
-  '/about': typeof AboutRoute
   '/quill': typeof QuillRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/about' | '/quill'
+  fullPaths: '/' | '/quill'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/about' | '/quill'
-  id: '__root__' | '/' | '/about' | '/quill'
+  to: '/' | '/quill'
+  id: '__root__' | '/' | '/quill'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
-  AboutRoute: typeof AboutRoute
   QuillRoute: typeof QuillRoute
 }
 
@@ -66,13 +56,6 @@ declare module '@tanstack/react-router' {
       path: '/quill'
       fullPath: '/quill'
       preLoaderRoute: typeof QuillRouteImport
-      parentRoute: typeof rootRouteImport
-    }
-    '/about': {
-      id: '/about'
-      path: '/about'
-      fullPath: '/about'
-      preLoaderRoute: typeof AboutRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/': {
@@ -87,7 +70,6 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
-  AboutRoute: AboutRoute,
   QuillRoute: QuillRoute,
 }
 export const routeTree = rootRouteImport
